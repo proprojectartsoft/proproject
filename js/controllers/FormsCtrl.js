0da9a2d16bf0a12593efe1882863c2ce826dbfd5
Sergiu <sergiu.radu.nichitean@artsoft-consult.ro>
2016-10-16 23:16:38 +0300

    x

--- js/controllers/FormsCtrl.js
+++ js/controllers/FormsCtrl.js
@@ -1,67 +1,75 @@
 angular.module($APP.name).controller('FormsCtrl', [
-  '$scope',
-  '$stateParams',
-  'FormDesignService',
-  '$rootScope',
-  'CacheFactory',
-  'AuthService',
-  '$state',
-  '$ionicPopup',
-  '$ionicHistory',
-  '$anchorScroll',
-  '$ionicSideMenuDelegate',
-  'DbService',
-  'SyncService',
-  function ($scope, $stateParams, FormDesignService, $rootScope, CacheFactory, AuthService, $state, $ionicPopup, $ionicHistory, $anchorScroll, $ionicSideMenuDelegate, DbService, SyncService) {
+    '$scope',
+    '$stateParams',
+    'FormDesignService',
+    '$rootScope',
+    'CacheFactory',
+    'AuthService',
+    '$state',
+    '$ionicPopup',
+    '$ionicHistory',
+    '$anchorScroll',
+    '$ionicSideMenuDelegate',
+    function ($scope, $stateParams, FormDesignService, $rootScope, CacheFactory, AuthService, $state, $ionicPopup, $ionicHistory, $anchorScroll, $ionicSideMenuDelegate) {
 
-    $scope.$on('$ionicView.enter', function () {
-      $ionicHistory.clearHistory();
-      $ionicSideMenuDelegate.canDragContent(false);
-    });
-
-    $scope.isLoaded = false;
-    $scope.hasData = '';
-    $scope.categoryId = $stateParams.categoryId;
-    $rootScope.slideHeader = false;
-    $rootScope.slideHeaderPrevious = 0;
-    $rootScope.slideHeaderHelper = false;
-
-    $rootScope.$on('$stateChangeStart', function () {
-      $anchorScroll.yOffset = 0;
-    });
-
-    AuthService.me().then(function (user) {
-      if (user && user.active === false) {
-        var alertPopup = $ionicPopup.alert({
-          title: 'Error',
-          template: 'Your account has been de-activated. Contact your supervisor for further information.',
+        $scope.$on('$ionicView.enter', function () {
+            $ionicHistory.clearHistory();
+            $ionicSideMenuDelegate.canDragContent(false);
         });
 
-        AuthService.logout().success(function () {
-          $state.go('login');
-        }, function () {
+        $scope.isLoaded = false;
+        $scope.hasData = '';
+        $scope.categoryId = $stateParams.categoryId;
+        $rootScope.slideHeader = false;
+        $rootScope.slideHeaderPrevious = 0;
+        $rootScope.slideHeaderHelper = false;
+
+        $rootScope.$on('$stateChangeStart', function () {
+            $anchorScroll.yOffset = 0;
         });
-      }
-      else{
-        SyncService.sync_close();
-      }
-    })
+
+        AuthService.me().then(function (user) {
+            if (user && user.active === false) {
+                var alertPopup = $ionicPopup.alert({
+                    title: 'Error',
+                    template: 'Your account has been de-activated. Contact your supervisor for further information.',
+                });
+
+            }
+        }, function errorCallback(error) {
+            console.log(error, error.status);
+        });
 
 
+        $rootScope.formDesigns = [];
+        designsCache = DbService.get('designs')
+        angular.forEach(designsCache, function (aux) {
+            if (aux.category_id === parseInt($stateParams.categoryId)) {
+                $rootScope.formDesigns.push(aux);
+            }
+        });
+        $scope.isLoaded = true;
+        if ($rootScope.formDesigns.length === 0) {
+            $scope.hasData = 'no data';
+        }
 
 
+        $scope.categoryName = $rootScope.categories[$stateParams.categoryId - 1].name;
 
+        $scope.refresh = function () {
+          $rootScope.formDesigns = [];
+          designsCache = DbService.get('designs')
+          angular.forEach(designsCache, function (aux) {
+              if (aux.category_id === parseInt($stateParams.categoryId)) {
+                  $rootScope.formDesigns.push(aux);
+              }
+          });
+        };
 
-    $scope.categoryName = $rootScope.categories[$stateParams.categoryId - 1].name;
-
-    $scope.refresh = function () {
-      SyncService.sync_button()
-    };
-
-    $scope.fixScroll = function () {
-      $rootScope.slideHeader = false;
-      $rootScope.slideHeaderPrevious = 0;
-      $rootScope.slideHeaderHelper = false;
+        $scope.fixScroll = function () {
+            $rootScope.slideHeader = false;
+            $rootScope.slideHeaderPrevious = 0;
+            $rootScope.slideHeaderHelper = false;
+        }
     }
-  }
 ]);
