7f624c1946dc94b9fff45c74a2622fb9e9bcaf08
Sergiu <sergiu.radu.nichitean@artsoft-consult.ro>
2016-10-17 22:47:28 +0300

    x

--- js/controllers/FormsCtrl.js
+++ js/controllers/FormsCtrl.js
@@ -1,77 +1,80 @@
 angular.module($APP.name).controller('FormsCtrl', [
-    '$scope',
-    '$stateParams',
-    'FormDesignService',
-    '$rootScope',
-    'CacheFactory',
-    'AuthService',
-    '$state',
-    '$ionicPopup',
-    '$ionicHistory',
-    '$anchorScroll',
-    '$ionicSideMenuDelegate',
-    'SyncService',
-    'DbService',
-    function ($scope, $stateParams, FormDesignService, $rootScope, CacheFactory, AuthService, $state, $ionicPopup, $ionicHistory, $anchorScroll, $ionicSideMenuDelegate, SyncService, DbService) {
+  '$scope',
+  '$stateParams',
+  'FormDesignService',
+  '$rootScope',
+  'CacheFactory',
+  'AuthService',
+  '$state',
+  '$ionicPopup',
+  '$ionicHistory',
+  '$anchorScroll',
+  '$ionicSideMenuDelegate',
+  'SyncService',
+  'DbService',
+  function ($scope, $stateParams, FormDesignService, $rootScope, CacheFactory, AuthService, $state, $ionicPopup, $ionicHistory, $anchorScroll, $ionicSideMenuDelegate, SyncService, DbService) {
 
-        $scope.$on('$ionicView.enter', function () {
-            $ionicHistory.clearHistory();
-            $ionicSideMenuDelegate.canDragContent(false);
+    $scope.$on('$ionicView.enter', function () {
+      $ionicHistory.clearHistory();
+      $ionicSideMenuDelegate.canDragContent(false);
+    });
+
+    $scope.isLoaded = false;
+    $scope.hasData = '';
+    $scope.categoryId = $stateParams.categoryId;
+    $rootScope.slideHeader = false;
+    $rootScope.slideHeaderPrevious = 0;
+    $rootScope.slideHeaderHelper = false;
+
+    $rootScope.$on('$stateChangeStart', function () {
+      $anchorScroll.yOffset = 0;
+    });
+
+    AuthService.me().then(function (user) {
+      if (user && user.active === false) {
+        var alertPopup = $ionicPopup.alert({
+          title: 'Error',
+          template: 'Your account has been de-activated. Contact your supervisor for further information.',
         });
 
-        $scope.isLoaded = false;
-        $scope.hasData = '';
-        $scope.categoryId = $stateParams.categoryId;
-        $rootScope.slideHeader = false;
-        $rootScope.slideHeaderPrevious = 0;
-        $rootScope.slideHeaderHelper = false;
-
-        $rootScope.$on('$stateChangeStart', function () {
-            $anchorScroll.yOffset = 0;
-        });
-
-        AuthService.me().then(function (user) {
-            if (user && user.active === false) {
-                var alertPopup = $ionicPopup.alert({
-                    title: 'Error',
-                    template: 'Your account has been de-activated. Contact your supervisor for further information.',
-                });
-
-            }
-        }, function errorCallback(error) {
-            console.log(error, error.status);
-        });
+      }
+    }, function errorCallback(error) {
+      console.log(error, error.status);
+    });
 
 
-        // $rootScope.formDesigns = [];
-        $rootScope.formDesigns = DbService.list_design($stateParams.categoryId)
-        // angular.forEach(designsCache, function (aux) {
-        //     if (aux.category_id === parseInt($stateParams.categoryId)) {
-        //         $rootScope.formDesigns.push(aux);
-        //     }
-        // });
-        $scope.isLoaded = true;
-        if ($rootScope.formDesigns.length === 0) {
-            $scope.hasData = 'no data';
+    // $rootScope.formDesigns = [];
+    $ionicPlatform.ready(function () {
+      $rootScope.formDesigns = DbService.list_design($stateParams.categoryId)
+      $scope.isLoaded = true;
+      if ($rootScope.formDesigns.length === 0) {
+        $scope.hasData = 'no data';
+      }
+    })
+    // angular.forEach(designsCache, function (aux) {
+    //     if (aux.category_id === parseInt($stateParams.categoryId)) {
+    //         $rootScope.formDesigns.push(aux);
+    //     }
+    // });
+
+
+
+    $scope.categoryName = $rootScope.categories[$stateParams.categoryId - 1].name;
+
+    $scope.refresh = function () {
+      $rootScope.formDesigns = [];
+      designsCache = DbService.get('designs')
+      angular.forEach(designsCache, function (aux) {
+        if (aux.category_id === parseInt($stateParams.categoryId)) {
+          $rootScope.formDesigns.push(aux);
         }
+      });
+    };
 
-
-        $scope.categoryName = $rootScope.categories[$stateParams.categoryId - 1].name;
-
-        $scope.refresh = function () {
-          $rootScope.formDesigns = [];
-          designsCache = DbService.get('designs')
-          angular.forEach(designsCache, function (aux) {
-              if (aux.category_id === parseInt($stateParams.categoryId)) {
-                  $rootScope.formDesigns.push(aux);
-              }
-          });
-        };
-
-        $scope.fixScroll = function () {
-            $rootScope.slideHeader = false;
-            $rootScope.slideHeaderPrevious = 0;
-            $rootScope.slideHeaderHelper = false;
-        }
+    $scope.fixScroll = function () {
+      $rootScope.slideHeader = false;
+      $rootScope.slideHeaderPrevious = 0;
+      $rootScope.slideHeaderHelper = false;
     }
+  }
 ]);
